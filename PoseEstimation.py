7d230f86b22096b4493bdde8fc72bf6475ef867a
Siddharth Narayana <siddharthvader101@gmail.com>
2025-12-30 16:05:54 -0500

    Time to work

--- PoseEstimation.py
+++ PoseEstimation.py
@@ -58,8 +58,25 @@ class poseDetector():
                         cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 255), 2)
         return angle
 
-
-# HAHA NO MORE MAIN 
+        
+def main():
+    cap = cv2.VideoCapture('PoseVideos/1.mp4')
+    pTime = 0
+    detector = poseDetector()
+    while True:
+        success, img = cap.read()
+        img = detector.findPose(img)
+        lmList = detector.findPosition(img, draw=False)
+        if len(lmList) != 0:
+            print(lmList[14])
+            cv2.circle(img, (lmList[14][1], lmList[14][2]), 15, (0, 0, 255), cv2.FILLED)
+        cTime = time.time()
+        fps = 1 / (cTime - pTime)
+        pTime = cTime
+        cv2.putText(img, str(int(fps)), (70, 50), cv2.FONT_HERSHEY_PLAIN, 3,
+                    (255, 0, 0), 3)
+        cv2.imshow("Image", img)
+        cv2.waitKey(1)
 
         
 if __name__ == "__main__":
